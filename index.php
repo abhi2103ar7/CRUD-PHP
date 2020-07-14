69b7f2d8acd575264d2bc5aa2451286d1d32f94a
abhi2103ar7 <abhishekranjan@karunya.edu.in>
2020-07-14 17:23:10 +0530

    Displaying data from the table

--- index.php
+++ index.php
@@ -11,24 +11,56 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
 </head>
 <body>
-    <?php
-        session_start();
-        if(isset($_SESSION['display'])){
-            echo "Done";
-        }
-    ?>
-
+    <div class="container h-100">
+        <div class="row row-header row-content align-items-center mt-4">
+            <div class="col-4">
+                <H3>NAME</H3>
+            </div>
+            <div class="col-4">
+                <H3>LOCATION</H3>
+            </div>
+            <div class="col-4">
+                
+            </div>
+        </div>
+        <?php
+            session_start();
+            if(isset($_SESSION['display'])){
+                $connection=mysqli_connect("localhost","root","","crud");
+                $display="SELECT * FROM data ";
+                $result=mysqli_query($connection,$display);
+                while($row=mysqli_fetch_array($result))
+                {
+                    echo '<div class="row row-header row-content align-items-center mt-4">
+                            <div class="col-4">'.
+                                $row['Name'].'
+                            </div>
+                            <div class="col-4">'.
+                                $row['Location'].'
+                            </div>
+                            <div class="col-4">
+                                <form action="variable.php" method="post">
+                                    <button type="submit" name="edit" class="btn btn-success mr-4">EDIT</button>
+                                    <button type="submit" name="delete" class="btn btn-danger">DELETE</button>       
+                                </form>
+                            </div>
+                          </div>';
+                }
+            }
+        ?>
+        </div>
+    </div>
 
     <div class="container">
-        <div class="row row-header">
+        <div class="row row-header mt-5">
             <div class="col-12 col-sm-6 offset-3">
             <form action="variable.php" method="POST">
                 <div class="form-group">
-                    <label>Name</label>
+                    <label class="font-weight-bold">Name</label>
                     <input type="text" class="form-control" name="name" placeholder="Enter the name">
                 </div>
                 <div class="form-group">
-                    <label>Location</label>
+                    <label class="font-weight-bold">Location</label>
                     <input type="text" class="form-control" name="location" placeholder="Enter the location"><br>
                 <div>
                 <button type="submit" name="save" class="btn btn-primary">Save</button><br>    
